00a87dfdc8bcf1230fc6f4c7737a33f0c144cb31
SteamFunkateer <coreythomas71@gmail.com>
2014-11-23 19:17:37 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function executes the inverse of the mean
+##    set the value of the vector
+##    get the value of the vector
+##    set the value of the mean
+##    get the value of the mean
+
 
 makeCacheMatrix <- function(x = matrix()) {
+
+        m <- NULL
+        set <- function(y)
+                {
+                x <<- y
+                m <<- NULL
+                }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+                
 
 }
 
 
-## Write a short comment describing this function
+## This function executes the inverse of the mean
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        m <- x$getinverse()
+if(!is.null(m)) {
+message("getting cached data")
+return(m)
+}
+data <- x$get()
+m <- solve(data, ...)
+x$setinverse(m)
+m
+
 }
